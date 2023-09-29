d284b7c1a67f03d2db1443c629f0330c55beffe1
Habeeb Rahman K T <habeebrhmn112@gmail.com>
2023-09-29 15:40:51 +0530

    Initial commit

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-<<<<<<< HEAD
-=======
     print("I am habeeb")
->>>>>>> e48d222 (Initial commit)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
